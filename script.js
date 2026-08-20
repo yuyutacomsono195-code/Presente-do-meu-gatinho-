e3e4d9b044c998c22d229330d99cb03560a9431c
yuyutacomsono195-code <yuyutacomsono195@gmail.com>
2026-08-20 15:28:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const botao = document.getElementById('botaoCoracao');
+const texto = document.getElementById('texto');
+
+botao.addEventListener('click', function() {
+    texto.innerHTML = "<strong>Eu te amo, Ezaias! ❤️</strong>";
+    texto.style.color = "#d63384";
+    texto.style.fontSize = "22px";
+});
 
